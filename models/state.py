8efd694207faccf9bb281adfb478729ad7a729c2
joshua530 <joshuaomari5@gmail.com>
2022-10-15 14:38:34 +0300

    make pep8 compliant

--- models/state.py
+++ models/state.py
@@ -9,20 +9,22 @@ from sqlalchemy.orm import relationship
 from models.city import City
 
 
-
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(60), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+        cities = relationship(
+            "City", backref="state", cascade="all, delete, delete-orphan")
     else:
         name = ""
+
         @property
         def cities(self):
             """
-            returns the list of City instances with state_id equals to the current State.id
+            returns the list of City instances with state_id equals
+            to the current State.id
             """
             cities = []
             for city in models.storage.all(City).values():
