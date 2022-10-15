ebb45b58f0d512ded19e26a3ddc56d15c3893921
joshua530 <joshuaomari5@gmail.com>
2022-10-15 14:13:07 +0300

    update script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Runs simple flask application with / route only"""
-from flask import Flask
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Home route"""
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """display text"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
